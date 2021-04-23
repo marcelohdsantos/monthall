c0d9b82caa9a4b6b47a0b5c3dd74f7e5a7d4fd95
Marcelo <marcelodossantos732@yhaoo.com.br>
2021-04-23 14:14:14 -0400

    estilização
    estilização da porta

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,43 @@
 <template>
   <div id="app">
       <h1>Problema de Monty Hall</h1>
-      <Gift />
+      <div class="form">
+          <div v-if="!started">
+              <label for="portsAmount">Quantas portas? </label>
+              <input type="text" id="portsAmount" size="3" v-model.number="portsAmount">
+          </div>
+
+          <div v-if="!started">
+              <label for="selectedPort"> Qual porta é a premiada? </label>
+              <input type="text" id="selectedPort" size="3" v-model.number="selectedPort">
+          </div>
+
+          <button v-if="!started" @click="started = true">Iniciar</button>
+          <button v-if="started" @click="started = false">Reiniciar</button>
+      </div>
+      <div class="doors" v-if="started">
+          <div v-for="i in portsAmount" :key="i">
+              <Door :hasGift="i === selectedPort" :number="i" />
+          </div>
+      </div>
   </div>
 </template>
 
 
 <script>
-import Gift from './components/Gift'
+    import Door from './components/Door'
 
-
-export default {
-    name: 'App',
-    components: { Gift }
-}
+    export default {
+        name: 'App',
+        components: { Door },
+        data: function() {
+            return {
+                started: false,
+                portsAmount: 3,
+                selectedPort: null
+            }
+        }
+    }
 
 </script>
 
@@ -41,5 +65,25 @@ body {
     background-color: #0004;
     padding: 20px;
     margin-bottom: 60px;
+}
+
+.form {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 40px;
+}
+
+.form, .form input, .form button {
+    margin-bottom: 10px;
+    font-size: 1.5rem;
+}
+
+.doors {
+    align-self: stretch;
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
 }
 </style>
